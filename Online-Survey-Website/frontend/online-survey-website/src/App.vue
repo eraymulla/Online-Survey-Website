f6b57ec62750ba1003912cdd7aff532749d63b93
eraymulla <eraymulla0@gmail.com>
2022-05-11 21:51:46 +0300

    Vue3 Vuex oluşturuldu.

---
+++ Online-Survey-Website/frontend/online-survey-website/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="div">
+    <h1 class=""> Flask API deneme </h1>
+    <label for="username">Username</label>
+    <input type="text" id="username" name="username"> <br>
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password"> <br>
+    <button type="button">Giriş yap</button>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
